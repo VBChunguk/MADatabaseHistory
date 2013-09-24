37a9df1c43d56ff63ec4bc2683c8238b067557d6
VBChunguk <chwo9843@gmail.com>
2013-09-24 18:48:51 +0900

    Add database watch service base
    Working on issue #3

---
+++ src/com/zotca/vbc/dbhistory/service/DatabaseWatchService.java
@@ -0,0 +1,19 @@
+package com.zotca.vbc.dbhistory.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class DatabaseWatchService extends Service {
+
+	@Override
+	public void onCreate() {
+	}
+	
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
